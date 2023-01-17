99af410d16052c6a3baa28aada218debb493d2e3
Dokimion0 <leecy3610@gmail.com>
2023-01-17 22:12:15 +0900

    feat: todo 수정 기능 추가

--- src/main.js
+++ src/main.js
@@ -2,35 +2,53 @@ import { readTodos, createTodo, updateTodo, deleteTodo } from './request.js';
 
 const formEl = document.querySelector('form');
 const inputEl = formEl.querySelector('input');
-const listEl = document.querySelector('.list');
+const todoContainer = document.querySelector('.todo-container');
 
 const renderTodos = (todos) => {
   const liEls = todos.map((todo) => {
     const liEl = document.createElement('li');
-    liEl.innerHTML = /* html */ `
-      <span>${todo.title}</span>
+    liEl.className = 'todo';
+    liEl.innerHTML = `
+      <div>
+        <span class="material-symbols-outlined">
+        radio_button_unchecked
+        </span>
+        <span contenteditable="true">${todo.title}</span>
+      </div>
+      <div class="todo__btn">
+        <input class="display-none" type ="text"/>
+        <button class="doneBtn">확인</button>
+        <button>삭제</button>
+      </div>
     `;
-    liEl.addEventListener('click', async () => {
-      await updateTodo(todo);
+
+    const updateBtn = liEl.querySelectorAll('button')[0];
+    const updateInput = liEl.querySelector('input');
+    const deleteBtn = liEl.querySelectorAll('button')[1];
+
+    updateBtn.addEventListener('click', async () => {
+      confirmBtn.classList.remove('display-none');
+      updateBtn.classList.add('display-none');
+      const newText = liEl.querySelector('input').value;
+      updateInput.classList.remove('display-none');
+
+      await updateTodo(todo, newText);
       const todos = await readTodos();
       renderTodos(todos);
     });
 
-    const btnEl = document.createElement('button');
-    btnEl.textContent = '삭제!';
-    btnEl.addEventListener('click', async () => {
+    deleteBtn.addEventListener('click', async () => {
       await deleteTodo(todo);
       const todos = await readTodos();
       renderTodos(todos);
     });
-    liEl.append(btnEl);
 
     return liEl;
   });
-  listEl.innerHTML = '';
-  listEl.append(...liEls);
-};
 
+  todoContainer.innerHTML = '';
+  todoContainer.append(...liEls);
+};
 
 let inputText = '';
 formEl.addEventListener('submit', async (event) => {
@@ -44,6 +62,5 @@ formEl.addEventListener('submit', async (event) => {
 
 (async () => {
   const todos = await readTodos();
-  console.log(todos);
   renderTodos(todos);
 })();
