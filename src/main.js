ae22b2213e0a4f8e2ac2ffc8888010aa309c6659
Dokimion0 <leecy3610@gmail.com>
2023-01-18 23:26:20 +0900

    refactor: todo 수정, todo  삭제 함수 추가
    clickTodosHandler 이벤트 위임 함수
    -startEditTodo
    -finishEditTodo
    -deleteEditTodo

--- src/main.js
+++ src/main.js
@@ -2,70 +2,88 @@ import { readTodos, createTodo, editTodo, deleteTodo } from './request.js';
 
 const formEl = document.querySelector('form');
 const inputEl = formEl.querySelector('input');
-const todoContainer = document.querySelector('.todo__container');
+const todosContainer = document.querySelector('.todos__container');
+
+const showTodos = async () => {
+  todosContainer.innerHTML = '';
+  const todos = await readTodos();
+  renderTodos(todos);
+};
+
+const startEditTodo = (todoContainer) => {
+  const editBtn = todoContainer.querySelector('.edit-btn');
+  editBtn.classList.add('display-none');
+
+  const doneBtn = todoContainer.querySelector('.done-btn');
+  doneBtn.classList.remove('display-none');
+
+  const editInput = todoContainer.querySelector('.edit-input');
+  editInput.classList.remove('display-none');
+
+  const todoText = todoContainer.querySelector('.todo-text');
+  todoText.classList.add('display-none');
+  editInput.value = todoText.textContent;
+};
+
+const finishEditTodo = async (todoContainer) => {
+  const id = todoContainer.dataset.id;
+
+  const editBtn = todoContainer.querySelector('.edit-btn');
+  editBtn.classList.remove('display-none');
+
+  const doneBtn = todoContainer.querySelector('.done-btn');
+  doneBtn.classList.add('display-none');
+
+  const editInput = todoContainer.querySelector('.edit-input');
+  const newText = editInput.value;
+
+  await editTodo(id, newText);
+  showTodos();
+};
+
+const deleteEditTodo = async (todoContainer) => {
+  const id = todoContainer.dataset.id;
+  await deleteTodo(id);
+  showTodos();
+};
+
+const clickTodosHandler = async (e) => {
+  const todoContainer = e.target.closest('.todo-container');
+  console.log(todoContainer);
+  if (!todoContainer) {
+    return;
+  }
+  if (e.target.matches('.edit-btn')) {
+    startEditTodo(todoContainer);
+  }
+  if (e.target.matches('.done-btn')) {
+    finishEditTodo(todoContainer);
+  }
+  if (e.target.matches('.delete-btn')) {
+    deleteEditTodo(todoContainer);
+  }
+};
 
 const renderTodos = (todos) => {
-  const liEls = todos.map((todo) => {
-    const liEl = document.createElement('li');
-    liEl.className = 'todo';
-    liEl.innerHTML = `
-      <div>
-        <span class="material-symbols-outlined">
-        radio_button_unchecked
-        </span>
-        <span class="text">${todo.title}</span>
-        <input class="display-none" type ="text"/>
-      </div>
-      <div class="todo__btn">
-        <button class="edit-Btn">
-          <span class="material-symbols-outlined">edit</span>
-        </button>
-        <button class="done-Btn display-none">
-          <span class="material-symbols-outlined">done</span>
-        </button>
-        <button>
-          <span class="material-symbols-outlined">delete</span>
-        </button>
-      </div>
-    `;
-    let newText = '';
-
-    const todoText = liEl.querySelector('.text');
-    const editInput = liEl.querySelector('input');
-    const editBtn = liEl.querySelectorAll('button')[0];
-    const doneBtn = liEl.querySelectorAll('button')[1];
-    const deleteBtn = liEl.querySelectorAll('button')[2];
-
-    editBtn.addEventListener('click', async () => {
-      doneBtn.classList.remove('display-none');
-      editBtn.classList.add('display-none');
-      editInput.classList.remove('display-none');
-      todoText.classList.add('display-none');
-
-      editInput.value = `${todo.title}`;
-    });
-
-    doneBtn.addEventListener('click', async () => {
-      doneBtn.classList.add('display-none');
-      editBtn.classList.remove('display-none');
-
-      newText = editInput.value;
-      await editTodo(todo, newText);
-      const todos = await readTodos();
-      renderTodos(todos);
-    });
-
-    deleteBtn.addEventListener('click', async () => {
-      await deleteTodo(todo);
-      const todos = await readTodos();
-      renderTodos(todos);
-    });
-
-    return liEl;
-  });
-
-  todoContainer.innerHTML = '';
-  todoContainer.append(...liEls);
+  const todosEl = todos
+    .map(
+      (todo) =>
+        `<li class="todo-container" data-id=${todo.id}>
+          <div>
+            <span class="material-symbols-outlined todo--toggle">radio_button_unchecked</span>
+            <span class="todo-text">${todo.title}</span>
+            <input class="edit-input display-none" type ="text"/>
+          </div>
+          <div class="todo-container__btn">
+            <span class="material-symbols-outlined edit-btn">edit</span>
+            <span class="material-symbols-outlined done-btn display-none">done</span>
+            <span class="material-symbols-outlined delete-btn">delete</span>
+          </div>
+        </li> 
+    `
+    )
+    .join('');
+  todosContainer.innerHTML += todosEl;
 };
 
 let inputText = '';
@@ -74,11 +92,13 @@ formEl.addEventListener('submit', async (event) => {
   inputText = inputEl.value;
 
   await createTodo(inputText);
-  const todos = await readTodos();
-  renderTodos(todos);
+
+  showTodos();
 });
 
 (async () => {
   const todos = await readTodos();
   renderTodos(todos);
 })();
+
+todosContainer.addEventListener('click', clickTodosHandler);
